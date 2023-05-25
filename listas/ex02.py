06c37dd6e27cc3da2c00d82370b584ce935f3f8c
Kauã <kauahaymon@hotmail.com>
2023-05-24 22:20:02 -0300

    Create ex02.py

---
+++ listas/ex02.py
@@ -0,0 +1,7 @@
+# ACRESTAR UM NOVO ELEMENTO NO FINAL DA LISTA (.append) 
+
+nomes = ['ana','maria','carol','laura','joao', 'joaquim']
+
+nomes.append('murilo') # .append - acrescenta um elemento no final da lista 
+
+print(nomes)
