5acc101fd4bfa351318c6b521bc9cd9e495c159e
Kauã <kauahaymon@hotmail.com>
2023-05-24 22:20:27 -0300

    Create ex03.py

---
+++ listas/ex03.py
@@ -0,0 +1,7 @@
+# ATRIBUIR UM NOVO ELEMENTO (SEM REMOVER O ANTERIOR) 
+
+nomes = ['ana','maria','carol','laura','joao', 'joaquim']
+
+nomes.insert(0, 'vitoria') # escolho a posição para atribuir um novo elemento sem remover o anterior
+
+print(nomes)
