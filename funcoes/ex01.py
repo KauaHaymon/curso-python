c816256e1d8603471ea08ba93212f324b34df2a8
Kauã <kauahaymon@hotmail.com>
2023-05-24 22:26:42 -0300

    funcoes

---
+++ funcoes/ex01.py
@@ -0,0 +1,6 @@
+# linha de função
+
+def soma(a, b): # def define uma função (qualquer função(toda função em parênteses))
+  return a + b
+resultado = soma(5,10)
+print(resultado)
---
+++ funcoes/ex02.py
@@ -0,0 +1,10 @@
+# SOMA NÚMEROS
+
+a = 10
+b = 14
+c = 20
+def soma(a, b, c):
+  return a + b + c
+
+result = soma(a, b, c)
+print(result) 
