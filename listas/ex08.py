b996aa36d61c35368ca2f60bb419ce74bc88969e
Kauã <kauahaymon@hotmail.com>
2023-05-24 22:22:26 -0300

    Create ex08.py

---
+++ listas/ex08.py
@@ -0,0 +1,7 @@
+# ALTERAR E ORDENAR A LISTA 
+
+nomes = ['ana','maria','carol','laura','joao', 'joaquim']
+
+nomes.sort() # ordena e altera a lista, diferente da tupla que apenas exibia a ordem
+
+print(nomes)
