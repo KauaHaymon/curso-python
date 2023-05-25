fd4d7a3dc70da1bc22745b2e083f0768b5201bdc
Kauã <kauahaymon@hotmail.com>
2023-05-24 22:23:01 -0300

    Create ex09.py

---
+++ listas/ex09.py
@@ -0,0 +1,9 @@
+# INVERTER A ORDEM DA LISTA 
+
+nomes = ['ana','maria','carol','laura','joao', 'joaquim']
+numero = list(range(0,21))
+numero.sort(reverse = True) #inverte a lista
+nomes.sort(reverse = True) 
+ 
+print(numero)
+print(nomes)
