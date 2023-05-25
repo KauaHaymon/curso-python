2ca74b5429af75a0c3e8842f697d336c70053752
Kauã <kauahaymon@hotmail.com>
2023-05-24 22:30:09 -0300

    created exs

---
+++ dicionarios/ex01.py
@@ -0,0 +1,15 @@
+# CONCEITOS
+
+contatos = [('maria','98822-2982'),('joão','91828-2938'),('claudio','92833-3398'),('pedro','87342-1928')]
+print(contatos[0][1])
+
+contatos = [('maria','98822-2982'),('joão','91828-2938'),('claudio','92833-3398'),('pedro','87342-1928')]
+# listaContatos=dict(contatos)
+
+print(contatos['maria' in contatos])
+
+telefones = {'maria':'98822-2982','joão':'91828-2938','claudio':'92833-3398','pedro':'87342-1928'}
+
+telefones = dict(telefones)
+
+print(telefones['maria'])
---
+++ dicionarios/ex02.py
@@ -0,0 +1,12 @@
+# CONCEITOS
+
+dados = {'nome':'kaua','idade':18} #nome, idade = CHAVE / 'kaua','18' = VALORES
+
+print(dados['nome'])
+print(dados)
+
+#print(dados.items()) # mostras todos os itens que exite no dicionario ex: ('nome','kaua'),('idade',18)
+
+# print(dados.keys()) # mostra as chaves do dicionarios ex: ('nome','idade')
+
+# print(dados.values()) # mostra os valores dos dicionarios ex: ('kaua','18')
