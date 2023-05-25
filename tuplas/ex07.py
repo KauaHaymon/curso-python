018153999a498a6f77edd33985ca204d5f5eb0a9
Kauã <kauahaymon@hotmail.com>
2023-05-24 22:17:33 -0300

    CREATED EXS

---
+++ tuplas/ex07.py
@@ -0,0 +1,5 @@
+# MOSTRA OS VALORES EM ORDEM
+
+A = (5,10,15,20)
+for cont in A:
+  print(cont)
---
+++ tuplas/ex08.py
@@ -0,0 +1,8 @@
+#ESPECIFICA O INÍCIO DA CONTAGEM
+
+A = (5,10,15,20)
+for cont in range(0,len(A)):
+  print(A[cont], A[cont])
+
+pessoa = ('Raquel',32,'Desenvolvimento de sistemas')
+print(pessoa)
